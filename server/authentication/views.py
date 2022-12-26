f48da0544f642076b86863f0b1463e583591cce2
MashodRana <mashod0rana@gmail.com>
2022-12-26 10:09:46 +0600

    view and url for test apis

--- server/authentication/views.py
+++ server/authentication/views.py
@@ -3,7 +3,8 @@ from django.http import JsonResponse
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import  Response
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
@@ -19,6 +20,19 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+class TestEndPoint(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        data = f"Congratulation {request.user}, your API just responded to GET request"
+        return Response({'response': data}, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        text = request.POST.get('text')
+        data = f'Congratulation your API just responded to POST request with text: {text}'
+        return Response({'response': data}, status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
