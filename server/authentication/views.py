2f84b6232d48d075856db54845b9246480766073
MashodRana <mashod0rana@gmail.com>
2022-12-26 09:40:44 +0600

    updated

--- server/authentication/views.py
+++ server/authentication/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import  Response
+from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from authentication.serializers import MyTokenObtainPairSerializer, RegisterSerializer
+
 
 # Create your views here.
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/token/',
+        '/api/register/',
+        '/api/token/refresh'
+    ]
+    return Response(routes)
+
